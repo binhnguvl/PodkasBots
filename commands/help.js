21eaa966cc4f52ac20f5c2ab9aaeb3410f3102a3
binhnguvl <91358109+binhnguvl@users.noreply.github.com>
2021-10-02 11:30:03 +0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -14,9 +14,9 @@ module.exports = {
         .addFields(
 		    { name: '❯ MODERATION [8]', value: '`kick`: Dùng để kick member\n`mute`: Dùng để mute người chơi\n`unmute`: Dùng để tắt mute người bị mute\n`ban`: Dùng để cấm người chơi\n`unban`: Dùng để bỏ cấm người bị cấm\n`clear`: Dùng để clear message\n`warn`: Dùng để cảnh cáo người chơi\n`addrole`: Dùng để addrole\n`removerole`: Dùng để xóa vai trò'},
         { name: '❯ INFO [4]', value: '`avatar`: Dùng để xem avatar\n`membercount`: Dùng để xem số người trong máy chủ\n`serverinfo`: Dùng để xem thông tin máy chủ\n`server-avatar`: Dùng để xem avatar máy chủ'},
-        { name: '❯ FUN [13]', value: '`backdoorsv`: Dùng để backdoorsv\n`bantinh`: Dùng để bắn t*nh\n`busku`: Dùng để bú cu\n`chat`: Làm cái gì mà chat mỗi 1 khu vậy?\n`cm`: Dùng để kiểm tra độ dài cu\n`djt`: Dùng để djt người chơi\n`rut`: Dùng để rút cu và bantinh lên mặt\n`uongtinh`: Dùng để uống t*nh\n`vieng`: Viếng chùa\n`xin`: Dùng để ăn xin\n`xinloi`: Dùng để xin lổi Onii\n`xinloi2`: Dùng để xin lổi\n`xinloi3`: Dùng để xin lỗi onii pt2'},
+        { name: '❯ FUN [12]', value: '`bantinh`: Dùng để bắn t*nh\n`busku`: Dùng để bú cu\n`chat`: Làm cái gì mà chat mỗi 1 khu vậy?\n`cm`: Dùng để kiểm tra độ dài cu\n`djt`: Dùng để djt người chơi\n`rut`: Dùng để rút cu và bantinh lên mặt\n`uongtinh`: Dùng để uống t*nh\n`vieng`: Viếng chùa\n`xin`: Dùng để ăn xin\n`xinloi`: Dùng để xin lổi Onii\n`xinloi2`: Dùng để xin lổi\n`xinloi3`: Dùng để xin lỗi onii pt2'},
         { name: '❯ GIVEAWAYS [1]', value: '`giveaways`: Dùng để mở giveaway'},
-        { name: '❯ MISC [4]', value: '`everyone`: Dùng để ping everyone\n`queue`: Dùng để check queue 2y2c\n`random`: Dùng để random number\n`say`: say command\n`ticket`: tạo ticket'},
+        { name: '❯ MISC [4]', value: '`everyone`: Dùng để ping everyone\n`queue`: Dùng để check queue 2y2c (fake)\n`random`: Dùng để random number\n`say`: say command'},
 	     )
         .setImage('')
         .setFooter(
