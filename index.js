fa1d9569bc9a50643b2e391a02f3852be7687cde
binhnguvl <91358109+binhnguvl@users.noreply.github.com>
2021-10-02 11:29:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -442,62 +442,6 @@ client.on('message', message => {
   }
 })
 
-
-client.on('message', async (message, args, cmd, client, discord) => {
-  if (message.content.startsWith(`${prefix}ticket`)) {
- 
-    const channel = await message.guild.channels.create(`ticket: ${message.author.tag}`);
-    
-    channel.setParent("891674264049172521");
-
-    channel.updateOverwrite(message.guild.id, {
-      SEND_MESSAGE: false,
-      VIEW_CHANNEL: false,
-    });
-    channel.updateOverwrite(message.author, {
-      SEND_MESSAGE: true,
-      VIEW_CHANNEL: true,
-    });
-
-    const reactionMessage = await channel.send("Thank you for contacting support!");
-
-    try {
-      await reactionMessage.react("🔒");
-      await reactionMessage.react("⛔");
-    } catch (err) {
-      channel.send("Error sending emojis!");
-      throw err;
-    }
-
-    const collector = reactionMessage.createReactionCollector(
-      (reaction, user) => message.guild.members.cache.find((member) => member.id === user.id).hasPermission("ADMINISTRATOR"),
-      { dispose: true }
-    );
-
-    collector.on("collect", (reaction, user) => {
-      switch (reaction.emoji.name) {
-        case "🔒":
-          channel.updateOverwrite(message.author, { SEND_MESSAGES: false });
-          break;
-        case "⛔":
-          channel.send("Deleting this channel in 5 seconds!");
-          setTimeout(() => channel.delete(), 5000);
-          break;
-      }
-    });
-
-    message.channel
-      .send(`We will be right with you! ${channel}`)
-      .then((msg) => {
-        setTimeout(() => msg.delete(), 7000);
-        setTimeout(() => message.delete(), 3000);
-      })
-      .catch((err) => {
-        throw err;
-      })
-}
-})
-
 client.on('message', (message) => {
    if (message.content.startsWith(`${prefix}userinfo`)) {
          // get the list of permissions
