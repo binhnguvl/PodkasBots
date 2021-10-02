f0a2c547604a32ba92065c43a4543d78e0f17f1f
binhnguvl <91358109+binhnguvl@users.noreply.github.com>
2021-10-02 11:20:34 +0700

    Delete backdoorsv.js

--- commands/backdoorsv.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'backdoorsv',
-    description: "keybackdoor",
-    execute(message, args){
-            message.channel.send(`${message.author} https://www.youtube.com/watch?v=dQw4w9WgXcQ`)
-            return message.channel.send('ngu vãi lz back back cdmm unmute cho t, unmute cho unmute cho t unmute cho t unmute unmute unmute unmute unmute unmute unmute unmute unmute unmute unmute unmute unmute unmute unmute unmute unmute unmute unmute unmute unmute unmute unmute unmute unmute unmute unmute unmute unmute unmute unmute unmute unmute unmute unmute ')
-        }
-    }
